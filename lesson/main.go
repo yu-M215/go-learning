16a6bbf357b651297c6b54cd22bbcfdd0e6ffff1
yu-M215 <yuu0609km.215@gmail.com>
2023-05-29 00:49:25 +0900

    明示的な変数の使い方

--- lesson/main.go
+++ lesson/main.go
@@ -12,4 +12,21 @@ import (
 func main() {
 	fmt.Println("Hello, World!!")
 	fmt.Println(time.Now())
+
+	// 明示的な定義
+	// var 変数名 型 = 値
+	var i int = 100
+	fmt.Println(i)
+
+	var s string = "Hello, Go"
+	fmt.Println(s)
+
+	var t, f bool = true, false
+	fmt.Println(t,f)
+
+	var (
+		i2 int = 200
+		s2 string = "Golang"
+	)
+	fmt.Println(i2, s2)
 }
