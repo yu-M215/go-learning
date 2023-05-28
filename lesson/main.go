652f2f71e55f83d6bc38d5f36a79af02b24a77aa
yu-M215 <yuu0609km.215@gmail.com>
2023-05-29 00:58:30 +0900

    暗黙的な型定義

--- lesson/main.go
+++ lesson/main.go
@@ -7,9 +7,12 @@ import (
 	"time"
 )
 
-// HelloWorld
+// 変数
+// i5 := 500
+var i5 int = 500
 
 func main() {
+	// HelloWorld
 	fmt.Println("Hello, World!!")
 	fmt.Println(time.Now())
 
@@ -29,4 +32,28 @@ func main() {
 		s2 string = "Golang"
 	)
 	fmt.Println(i2, s2)
+
+	var i3 int // int の初期値は０
+	var s3 string // string の初期値は空文字
+	fmt.Println(i3, s3)
+
+	i3 = 300
+	s3 = "Go"
+	fmt.Println(i3, s3)
+
+	i = 150
+	fmt.Println(i)
+
+	// 暗黙的な定義
+	// 変数名 := 値
+	i4 := 400
+	fmt.Println(i4)
+
+	// i4 := 500
+	// fmt.Println(i4)
+
+	// i4 = "Hello"
+	// fmt.Println(i4)
+
+	fmt.Println(i5)
 }
