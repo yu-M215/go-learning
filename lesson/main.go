5990814734682203c2fff4a8f356fbd68c1044d3
yu-M215 <yuu0609km.215@gmail.com>
2023-05-29 00:18:50 +0900

    重複ファイルの削除

--- lesson/main.go
+++ lesson/main.go
@@ -1,9 +1,15 @@
+// package の宣言は一つだけ。複数使いたい場合は、importして使用する。
 package main
 
-import "fmt"
+// パッケージのimportは複数可能。() で複数記述できる。
+import (
+	"fmt"
+	"time"
+)
 
-// HelloWorkd
+// HelloWorld
 
 func main() {
 	fmt.Println("Hello, World!!")
+	fmt.Println(time.Now())
 }
